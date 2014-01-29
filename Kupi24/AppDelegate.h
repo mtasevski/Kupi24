e04af1421a5e4afa7a218347a70dbea35fd9d25d
Milena Tasev <milenatasev@Milena-Tasevs-MacBook.local>
2014-01-29 16:49:23 +0100

    Initial Commit

---
+++ Kupi24/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Kupi24
+//
+//  Created by Milena Tasev on 1/29/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kupi24/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Kupi24
+//
+//  Created by Milena Tasev on 1/29/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
